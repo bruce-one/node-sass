2269125fe3f6f190e336b0d58013ba6ce99eada8
Bryce Gibson <bryce@gibson-consulting.com.au>
2017-03-17 22:47:03 +1100

    [publish] Use node-pre-gyp find.

--- test/binding.js
+++ test/binding.js
@@ -11,11 +11,10 @@ describe('binding', function() {
   describe('missing error', function() {
     it('should be useful', function() {
       process.env.SASS_BINARY_NAME = 'unknown-x64-48';
-
       assert.throws(
         function() { binding(etx); },
         function(err) {
-          var re = new RegExp('Missing binding.*?\\' + path.sep + 'vendor\\' + path.sep);
+          var re = new RegExp('Missing binding.*?');
           if ((err instanceof Error)) {
             return re.test(err);
           }
@@ -29,7 +28,7 @@ describe('binding', function() {
         function(err) {
           var etx = require('../lib/extensions');
 
-          delete process.env.SASS_BINARY_NAME;
+          delete process.env.SASS_BINARY_PATH;
 
           if ((err instanceof Error)) {
             return err.message.indexOf(
